34c03586c9501440c1c279d9747debb16eb56510
valentinpate <valentinp261@gmail.com>
2024-01-29 18:58:53 -0300

    hero start

---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,20 @@
+import '../App.css';
+import Header from './parts/Header';
+
+function Hero() {
+  return (
+    <div className="hero-bg">
+        <Header/>
+        <section className="hero">
+            <h1>Escribe lo que quieras y gratis.</h1>
+            <h2>Bienvenido a tu blog.</h2>
+            <div className="hero-btns">
+                {/* <a href="/signin"><button className="hero-btn signin-btn">Iniciar sesión</button></a> */}
+                <a href="/signup"><button className="hero-btn signup-btn">Haz tu primera entrada</button></a>
+            </div>
+        </section>
+    </div>
+  );
+}
+
+export default Hero;
