ed47755d48644e741fe1a3c9ca957eeb6ef21096
NicoLopezBjj <lopeznnr@gmail.com>
2024-02-01 12:47:39 -0300

    generate TOKEN and middleware passport-authenticate

--- server/config/passport-jwt.js
+++ server/config/passport-jwt.js
@@ -15,22 +15,32 @@ const opts = {
 
 module.exports.executeStrategy = function(passport) {
     console.log("entré a la estrategia")
-    passport.use(new JwtStrategy( opts , function(jwt_payload, done){ //función passport.use. no encerrar línea de passport.use en el try/catch
+    passport.use(new JwtStrategy( opts , async function(jwt_payload, done){ //función passport.use. no encerrar línea de passport.use en el try/catch
         console.log(jwt_payload)
         try{
-            Usuario.findOne( {_id:jwt_payload}, async function (err, usuario) { //función callback que busca el id en el payload de jwt
-                if (err) { //si hay un error
-                    return done (err, false) // done es un callback error-first, por lo que el error siempre va como primer parámetro. Si no hay error, el primer parámetro es null.
-                }
-                if (usuario) { //si el usuario está
-                    const match = await bcrypt.compare(jwt_payload, usuario.password) //jwt_payload tiene al usuario entrante, acordate!!
-                    if(match){
-                        return done(null , usuario)
-                    }  
-                } else { //si el usuario NO está
-                    return done (null, false)
-                }
-            } )
+            const usuario = await Usuario.findOne({_id:jwt_payload.sub})
+
+            if (!usuario) {
+                return done (null,false)
+            }
+
+            return done(null, usuario)
+
+            // const usuario = await Usuario.findOne( {_id:jwt_payload.sub}, async function (err, usuario) { //función callback que busca el id en el payload de jwt
+            //     if (err) { //si hay un error
+            //         return done (err, false) // done es un callback error-first, por lo que el error siempre va como primer parámetro. Si no hay error, el primer parámetro es null.
+            //     // }
+            //     // if (usuario) { //si el usuario está
+            //     //     const match = await bcrypt.compare(jwt_payload.password, usuario.password) //jwt_payload tiene al usuario entrante, acordate!!
+            //     //     if(match){
+            //     //         return done(null , usuario)
+            //     //     }  
+            //     } 
+                
+            //     else { //si el usuario NO está    
+            //         return done (null, false)
+            //     }
+            // } )
         }catch(err){ //error en el try
             console.log(err)
             return done(err, false)
